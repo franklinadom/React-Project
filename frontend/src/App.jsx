74904a220b8236c938af7962b141987a3e708654
Adom Franklin <franlinadom03@gmail.com>
2026-05-18 22:05:09 -0400

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,11 @@ import Header from './Header.jsx'
 function App() {
   
     return(
+      <>
+      
       <Header/>
+      
+      </>
     );
 }
 
--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -13,10 +13,27 @@ function Header(){
         </nav>
       </div>
 
-      <div>
-        <header>
-          <p className="Brand-name">JobMarket</p>
-        </header>
+      <div className="container">
+          <p className="brand-name">JobMarket</p>
+          
+          <div className="container2">
+          <p>Personal customers</p>
+          <p>Employers and entrepreneurs</p>
+          <p>Employment and statistics</p>
+          <p>Regional pages</p>
+          </div>
+          
+      </div>
+
+      <div className="section3">
+        <p>Evaluation and research</p>
+        <p>Statistics and reports</p>
+        <p>Job Market Finland in numbers</p>
+      </div>
+
+      <div className="main">
+        <h2 className="main-text">Employment and statistics</h2>
+        <p className="main-text2">The Employment and statistics' section contains a collection of assessments, surveys, studies, statistics, and reports published by the KEHA Center</p>
       </div>
       </>
     );
