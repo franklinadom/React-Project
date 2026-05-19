e67619e6f3b1900a16e1ae08e02b48560e266bd5
Peter Zorve <peterzorve28@gmail.com>
2026-05-19 13:21:18 +0300

    my version of the project

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,70 @@
+
+function Header(){
+
+    return(
+      <>
+      
+      <div>
+        <nav className="navbar">
+        <h5>search</h5>
+        <h5>Instructions and Support</h5>
+        <h5>English(EN)</h5>
+        <h5>Login</h5>
+        </nav>
+      </div>
+
+      <div className="container">
+          <p className="brand-name">JobMarket</p>
+          
+          <div className="container2">
+          <p>Personal customers</p>
+          <p>Employers and entrepreneurs</p>
+          <p>Employment and statistics</p>
+          <p>Regional pages</p>
+          </div>
+          
+      </div>
+
+      <div className="section3">
+        <p>Evaluation and research</p>
+        <p>Statistics and reports</p>
+        <p>Job Market Finland in numbers</p>
+      </div>
+
+      <div className="main">
+        <h2 className="main-text">Employment and statistics</h2>
+        <p className="main-text2">The Employment and statistics' section contains a collection of assessments, surveys, studies, statistics, and reports published by the KEHA Center</p>
+      </div>
+
+      <div className="second-body">
+        <div>
+          <p>The trend of the employment </p>
+          <p className="first">rate(People aged 15-64)</p>
+          <p className="percentage">71%</p>
+        </div>
+        
+        <div>
+          <p>Number of unemployed</p> 
+          <p className="second">persons</p>
+          <p className="figure1">342,800</p>
+        </div>
+
+        <div>
+          <p>New vacancies</p>
+          <p className="figure2">42,000</p>
+        </div>
+
+        <div>
+          <p>Published job applicant </p>
+          <p className="third">profiles</p>
+          <p className="figure3">47,200</p>
+        </div>
+
+    
+      </div>
+      </>
+    );
+
+}
+
+export default Header
