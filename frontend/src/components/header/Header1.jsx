348692c752e669ea8eb38b415797e912dd348d42
Adom Franklin <franlinadom03@gmail.com>
2026-05-22 18:07:42 -0400

    updated

--- frontend/src/components/header/Header1.jsx
+++ frontend/src/components/header/Header1.jsx
@@ -36,31 +36,41 @@ function Header1(){
 
       <div className="main">
         <h2 className="main-text">Employment and statistics</h2>
-        <p className="main-text2">The Employment and statistics' section contains a collection of assessments, surveys, studies, statistics, and reports published by the KEHA Center</p>
+        <p className="main-text2">The Employment and statistics' section contains a collection of assessments, surveys, studies, statistics, and reports published by the KEHA Center. </p>
       </div>
 
       <div className="second-body">
         <div>
           <p>The trend of the employment </p>
           <p className="first">rate(People aged 15-64)</p>
-          <p className="percentage">71%</p>
+          <p className="figure">71%</p>
+          <p className="decimal">- 0.6 pp</p>
+          <p className="sub1">compared to one year ago /</p>
+          <p className="sub2">Source: Statistics Finland</p> 
         </div>
         
         <div>
           <p>Number of unemployed</p> 
           <p className="second">persons</p>
-          <p className="figure1">342,800</p>
+          <p className="figure">342,800</p>
+          <p className='decimal1'>+ 22,400</p>
+          <p className="sub1">compared to one year ago /</p>
+          <p className="sub2">Source: KEHA centre</p>
         </div>
 
         <div>
           <p>New vacancies</p>
-          <p className="figure2">42,000</p>
+          <p className="figure">42,000</p>
+          <p className='decimal2'>- 4,900</p>
+          <p className="sub1">compared to one year ago /</p>
+          <p className="sub2">Source: KEHA centre</p>
         </div>
 
         <div>
           <p>Published job applicant </p>
           <p className="third">profiles</p>
-          <p className="figure3">47,200</p>
+          <p className="figure">47,200</p>
+          <p className="sub2">Source:job Market Finland</p>
         </div>
 
     
